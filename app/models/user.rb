c183626e9b2dec107bc33325efb84e70799a9b68
crespire <li.simmon@gmail.com>
2022-04-26 09:19:59 -0400

    Add Devise users

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true
+  validates :email, uniqueness: true
+  validates :email, presence: true
+  validates :email, format: Devise.email_regexp
+end
