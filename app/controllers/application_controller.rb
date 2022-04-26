347be3fdb0c82ddbe5e47a982b07568d07f7415d
crespire <li.simmon@gmail.com>
2022-04-26 09:10:06 -0400

    Add authentication before all actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+
+  protected
+
+  def authenticate_user!
+    if user_signed_in?
+      super
+    else
+      redirect_to login_path, notice: 'Please log in first.'
+    end
+  end
 end
