b4cdd830fe574b1aab3a86ef824d4553a465bb8b
crespire <li.simmon@gmail.com>
2022-05-04 15:39:26 -0400

    Update Likes controller to set user

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,12 @@
 class LikesController < ApplicationController
+  before_action :set_user, only: %i[ new edit update destroy ]
+
+  private
+
+  def set_user
+    @user = User.find(current_user.id)
+  end
+
+  def like_params
+  end
 end
