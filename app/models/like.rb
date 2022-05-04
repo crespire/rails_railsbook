bba46b830b520e1ac3b6bd028d8c1b9a22df61ba
crespire <li.simmon@gmail.com>
2022-05-04 15:28:19 -0400

    Add some validations

--- app/models/like.rb
+++ app/models/like.rb
@@ -3,4 +3,6 @@ class Like < ApplicationRecord
   belongs_to :user, foreign_key: :liked_by
 
   validates :liked_by, presence: true
+  validates :likeable_type, presence: true
+  validates :likeable_id, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -18,7 +18,7 @@ class User < ApplicationRecord
            foreign_key: :receiver_id,
            class_name: :Request
 
-  has_many :likes, class_name: :Like, foreign_key: :liked_by, dependent: :destroy
+  has_many :likes, foreign_key: :liked_by, dependent: :destroy
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
 
