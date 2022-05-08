4e67a6b1e0d7c6f93e63a6abb2fe003e7bb06299
crespire <li.simmon@gmail.com>
2022-05-08 17:57:45 -0400

    Turn off Turbo for Devise new_user_reg to continue system testing

--- spec/system/user_system_spec.rb
+++ spec/system/user_system_spec.rb
@@ -6,14 +6,27 @@ RSpec.describe 'User system', type: :system do
   end
 
   it 'registers a user when provided with good information' do
-    visit new_user_registration
+    visit '/users/sign_up'
 
-    fill_in 'email', with: 'test0@test.com'
-    fill_in 'name', with: 'test0'
-    fill_in 'password', with: 'password'
-    fill_in 'password confirmation', with: 'password'
-    click_button 'Sign Up'
+    fill_in 'Email', with: 'test0@test.com'
+    fill_in 'Name', with: 'test0'
+    fill_in 'Password', with: 'password'
+    fill_in 'Password confirmation', with: 'password'
+    click_button 'Sign up'
 
-    expect(page).to have_text('User was successfully created.')
+    expect(page).to have_text('Hello test0.')
+    expect(page).to have_text('You have signed up successfully.')
+    expect(User.count).to eq(1)
+  end
+
+  it 'does not allow a user to register with a blank email' do
+    visit '/users/sign_up'
+
+    fill_in 'Name', with: 'test0'
+    fill_in 'Password', with: 'password'
+    fill_in 'Password confirmation', with: 'password'
+    click_button 'Sign up'
+
+    expect(page).to have_text('Email is required.')
   end
 end
