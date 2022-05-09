e91d0ad6c79fecee68921b5f6defb591aa374623
crespire <li.simmon@gmail.com>
2022-05-09 17:44:18 -0400

    Update user system spec to validate client side errors

--- spec/system/user_system_spec.rb
+++ spec/system/user_system_spec.rb
@@ -27,6 +27,24 @@ RSpec.describe 'User system', type: :system do
     fill_in 'Password confirmation', with: 'password'
     click_button 'Sign up'
 
-    expect(page).to have_text('errors prohibited')
+    message = page.find('#user_email').native.attribute('validationMessage')
+
+    expect(message).to have_text('Please fill in this field.')
+    expect(current_path).to eq new_user_registration_path
+  end
+
+  it 'does not allow a user to register with a malformed email' do
+    visit '/users/sign_up'
+
+    fill_in 'Email', with: 'testbademail.com'
+    fill_in 'Name', with: 'test0'
+    fill_in 'Password', with: 'password'
+    fill_in 'Password confirmation', with: 'password'
+    click_button 'Sign up'
+
+    message = page.find('#user_email').native.attribute('validationMessage')
+
+    expect(message).to have_text('Please include')
+    expect(current_path).to eq new_user_registration_path
   end
 end
