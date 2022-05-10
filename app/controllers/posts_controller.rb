c9d4bc229dd7b4c180ed1717a3a78f906ef38c30
crespire <li.simmon@gmail.com>
2022-05-10 12:57:27 -0400

    Add stubs for Posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,4 +4,19 @@ class PostsController < ApplicationController
   def index
     @posts = Post.includes(:user).all
   end
+
+  def create
+  end
+
+  def update
+  end
+
+  def new
+  end
+
+  private
+
+  # Strong parameters
+  def post_params
+  end
 end
