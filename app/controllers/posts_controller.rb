cf86d3bcc9060021e9326d04058d8cfbee587e4c
crespire <li.simmon@gmail.com>
2022-05-10 13:57:53 -0400

    Finish Post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,22 +1,71 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: %i[edit update destroy]
   before_action :authenticate_user!
   
   def index
     @posts = Post.includes(:user).all
   end
 
-  def create
-  end
-
-  def update
-  end
-
   def new
+    @post = current_user.posts.build
+  end
+
+  # GET /posts/1/edit
+  def edit
+    redirect_to :root, alert: "Not authorized to edit another user's posts." unless @post.user.id == current_user.id
+  end
+
+  # POST /posts or /posts.json
+  def create
+    @post = current_user.posts.build(post_params)
+
+    respond_to do |format|
+      if @post.save
+        format.html { redirect_to :root, notice: "Post was successfully created." }
+        format.json { render :show, status: :created, location: @post }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /posts/1 or /posts/1.json
+  def update
+    return unless @post.user.id == current_user.id
+
+    respond_to do |format|
+      if @post.update(post_params)
+        format.html { redirect_to :root, notice: "Post was successfully updated." }
+        format.json { render :show, status: :ok, location: @post }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /posts/1 or /posts/1.json
+  def destroy
+    return unless @post.user.id == current_user.id
+
+    @post.destroy
+
+    respond_to do |format|
+      format.html { redirect_to :root, notice: "Post was successfully removed." }
+      format.json { head :no_content }
+    end
   end
 
   private
 
+  # Use callbacks to share common setup or constraints between actions.
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
   # Strong parameters
   def post_params
+    params.require(:post).permit(:title, :content)
   end
 end
