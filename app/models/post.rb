36714ac49f3ecdf65c7ca384f67de4a4ede175ae
crespire <li.simmon@gmail.com>
2022-05-11 15:48:14 -0400

    Add counter cache for comments on a post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :likes, as: :likeable, counter_cache: true
-  has_many :comments, as: :commentable
+  has_many :comments, as: :commentable, counter_cache: true
 
   validates :content, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_02_170522) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_11_194236) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -42,6 +42,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_02_170522) do
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
     t.integer "likes_count"
+    t.integer "comments_count"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
