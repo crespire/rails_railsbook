9340863dfe041fa997472abc23af4edd46bedae8
crespire <li.simmon@gmail.com>
2022-05-26 10:45:52 -0400

    Fix symbol list in Comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,6 @@
 class CommentsController < ApplicationController
   before_action :set_comment, only: %i[edit update destroy]
-  before_action :set_parent, only: %i[new, create]
+  before_action :set_parent, only: %i[new create]
   before_action :authenticate_user!
 
   def new
