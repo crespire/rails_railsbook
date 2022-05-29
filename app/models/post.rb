88f145637d172204e5d746204e15c06e31b7ebb1
crespire <li.simmon@gmail.com>
2022-05-29 14:26:22 -0400

    Add dependent destroy for post likes

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
-  has_many :likes, as: :likeable, counter_cache: true
+  has_many :likes, as: :likeable, counter_cache: true, dependent: :destroy
   has_many :comments, dependent: :destroy, counter_cache: true, inverse_of: :post
 
   validates :content, presence: true
