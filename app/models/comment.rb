83aabbbb753b0f8eb61cdf6f66b9725d4e259db3
crespire <li.simmon@gmail.com>
2022-05-29 14:26:49 -0400

    Add dependent destroy for likes on comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,7 @@ class Comment < ApplicationRecord
   belongs_to :post
   belongs_to :user
 
-  has_many :likes, as: :likeable
+  has_many :likes, as: :likeable, dependent: :destroy
 
   validates :content, presence: true
 end
