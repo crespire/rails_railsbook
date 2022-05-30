80da2b3a2aa63eba17618659b79dd6fbdaead30c
crespire <li.simmon@gmail.com>
2022-05-30 14:46:14 -0400

    Add two tests for create comment in comment system spec

--- spec/system/comment_system_spec.rb
+++ spec/system/comment_system_spec.rb
@@ -1,4 +1,4 @@
-equire 'rails_helper'
+require 'rails_helper'
 
 RSpec.describe 'Comment system', type: :system do
   before do
@@ -6,25 +6,46 @@ RSpec.describe 'Comment system', type: :system do
   end
 
   context 'when a user is logged in' do
-    it 'allows a user to comment on their own post' do
+    let(:user) { FactoryBot.create(:user) }
+
+    before do
+      login_as user, scope: :user
     end
 
-    it 'does not allow a user to submit a blank comment' do
-    end
+    context 'with a post from another user' do
 
-    it "allows a user to comment on another user's post" do
-    end
+      before do
+        create :post
+      end
 
-    it 'allows a user to edit their own comment' do
-    end
+      it 'allows a user to comment on the post' do
+        visit root_path
+        find('div.c-post__input').fill_in 'comment_content', with: 'Test Comment'
+        click_button 'Add comment'
 
-    it 'does not allow a user to update to a blank comment' do
-    end
+        expect(page).to have_text('Test Comment')
+      end
 
-    it 'does not allow a user to edit another users comment' do
-    end
+      it 'does not allow a user to submit a blank comment' do
+        visit root_path
+        find('div.c-post__input').click_button 'Add comment'
+        message = page.find('#comment_content').native.attribute('validationMessage')
 
-    it 'allows a user to delete their own comment' do
+        expect(message).to have_text('Please fill in this field.')
+        expect(current_path).to eq(root_path)
+      end
+
+      xit 'allows a user to edit their own comment' do
+      end
+  
+      xit 'does not allow a user to update to a blank comment' do
+      end
+  
+      xit 'does not allow a user to edit another users comment' do
+      end
+  
+      xit 'allows a user to delete their own comment' do
+      end
     end
   end
 end
