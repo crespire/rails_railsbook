8f32afba0b3d2123ac834dd464223cc584a71c6f
crespire <li.simmon@gmail.com>
2022-06-05 13:26:37 -0400

    Added the start of the requests controller

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,4 +1,19 @@
 class RequestsController < ApplicationController
   before_action :authenticate_user!
-  
+  before_action :set_user, only: %i[index]
+
+  # Show index of requests for current user
+  def index
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  # Strong Params
+  def request_params
+
+  end
 end
