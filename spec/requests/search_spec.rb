1cde0f1e3eb713a9102528a70cc26837b92b33b1
crespire <li.simmon@gmail.com>
2022-06-09 11:32:37 -0400

    Add request spec for search

---
+++ spec/requests/search_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe "Search", type: :request do
+  describe 'Exact match search' do
+    let(:searching_user) { FactoryBot.create(:user) }
+
+    before do
+      5.times { FactoryBot.create(:user) }
+      login_as(searching_user, scope: :user)
+    end
+
+    it 'returns a user when given the exact name' do
+      user = User.find(User.pluck(:id).sample)
+      get "/search?query=#{user.name}&commit=Search"
+      expect(response.body).to include(user.name)
+    end
+  end
+
+  describe 'Partial match search' do
+    let(:search_user) { FactoryBot.create(:user) }
+
+    before do
+      10.times { |i| FactoryBot.create(:user, name: "test#{i}") }
+      login_as(search_user, scope: :user)
+    end
+
+    it 'returns all partial matches' do
+      get '/search?query=test&commit=Search'
+      expect(response.body).to include('<li>').at_least(10).times
+    end
+  end
+end
