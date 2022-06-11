764a0ba7e0fec8e53077661929d689f11b4dfa87
crespire <li.simmon@gmail.com>
2022-06-11 11:27:58 -0400

    Add stub methods to Requests controller

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,20 +1,41 @@
 class RequestsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_user, only: %i[index]
+  before_action :set_user, only: %i[index create new]
+  before_action :set_request, only: %i[show edit update destroy]
 
   # Show index of requests for current user
   def index
     @requests = @user.all_requests
   end
 
+  def new
+    @request = Request.new
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  # Used to accept friendship, I wonder if we can alias 'update' with 'accept'
+  def update
+  end
+
   private
 
   def set_user
-    @user = User.find(params[:id])
+    @user = User.find(params[:user_id])
+  end
+
+  def set_request
+    @request = Request.find(params[:id])
   end
 
   # Strong Params
   def request_params
-
   end
 end
