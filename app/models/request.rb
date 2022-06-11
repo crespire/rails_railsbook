848eab0063bfbffe624c8e7f2755e11eb81cf4bd
crespire <li.simmon@gmail.com>
2022-06-11 15:19:19 -0400

    Add accepted and pending scopes

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,4 +1,7 @@
 class Request < ApplicationRecord
   belongs_to :requestor, class_name: :User
   belongs_to :receiver, class_name: :User
+
+  scope :accepted, -> { where('accepted = true') }
+  scope :pending, -> { where('accepted = false') }
 end
