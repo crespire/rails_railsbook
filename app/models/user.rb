012d6e8ad824c7a24484eb74d6e89871a59d6a2d
crespire <li.simmon@gmail.com>
2022-06-12 10:00:19 -0400

    Add dependent destory on scoped associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,8 @@ class User < ApplicationRecord
   validates :email, format: Devise.email_regexp
 
   has_many :requests, dependent: :destroy
-  has_many :friends, -> { merge(Request.accepted) }, through: :requests
-  has_many :pending_friends, -> { merge(Request.pending) }, source: :friend, through: :requests
+  has_many :friends, -> { merge(Request.accepted) }, through: :requests, dependent: :destroy
+  has_many :pending_friends, -> { merge(Request.pending) }, source: :friend, through: :requests, dependent: :destroy
 
   has_many :likes, foreign_key: :liked_by, dependent: :destroy
   has_many :posts, dependent: :destroy
