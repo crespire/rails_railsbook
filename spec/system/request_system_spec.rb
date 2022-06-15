46080acc24b13b3b6fa4a625e284e2ea5ab09289
crespire <li.simmon@gmail.com>
2022-06-15 14:41:42 -0400

    Add stubs for request system testing

---
+++ spec/system/request_system_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe 'User system', type: :system do
+  before do
+    driven_by(:selenium_chrome_headless)
+  end
+
+  context 'For a user initiating requests' do
+    it "updates the user's pending requests count" do
+    end
+
+    it "updates the target user's received requests" do
+    end
+
+    it 'allows a user to delete a pending request' do
+    end
+  end
+end
