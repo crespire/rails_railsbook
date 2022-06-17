f98b1d0044edaf5ce3619f3a6431e1bbf122bd12
crespire <li.simmon@gmail.com>
2022-06-17 17:25:57 -0400

    Start on some request system tests

--- spec/system/request_system_spec.rb
+++ spec/system/request_system_spec.rb
@@ -6,19 +6,54 @@ RSpec.describe 'Request/friend system', type: :system do
   end
 
   context 'For a user initiating requests' do
-    it "updates the user's pending requests count" do
+    let!(:user_a) { FactoryBot.create(:user) }
+    let!(:user_b) { FactoryBot.create(:user, name: 'Bob') }
+
+    before do
+      13.times { |i| FactoryBot.create(:user, name: "Susan #{i}") }
     end
 
-    it "updates the target user's received requests" do
+    it "updates all users' pending requests counts" do
+      login_as(user_a, scope: :user)
+      visit root_path
+      find('input#query').fill_in with: 'Bob'
+      click_button 'Search'
+
+      expect(page).to have_text('Send request').once
+      first('li').click_link('Send request')
+      expect(page).to have_text('Request sent!').once
+      expect(user_a.sent_requests.count).to eq(1)
+      expect(user_b.received_requests.count).to eq(1)
     end
 
     it 'allows a user to delete a pending request' do
+      login_as(user_a, scope: :user)
+      visit root_path
+      find('input#query').fill_in with: 'Bob'
+      click_button 'Search'
+      expect(page).to have_text('Bob').once
+      first('li').click_link('Send request')
+      expect(user_a.sent_requests.count).to eq(1)
+
+      click_link 'Requests'
+      expect(page).to have_text('Request sent to')
+      page.accept_alert do
+        find('li', text: 'Request sent to Bob').click_link('Delete')
+      end
+      expect(page).to have_text('Deleted!')
+      expect(user_a.sent_requests.count).to eq(0)
     end
 
-    it 'allows a user to delete an accepted request' do
+    xit 'shows a user pending requests to be accepted' do
     end
 
-    it 'does not allow a user to send a request to self' do
+    xit 'allows a user to accept a pending, received request' do
+    end
+    
+    xit 'allows a user to delete an accepted request' do
+    end
+
+    xit 'does not allow a user to send a request to self' do
       # log in as user, search for self, should not be accept button
     end
   end
