871edf16d3d2e1685ec69d2dc5dfeb40682225ef
crespire <li.simmon@gmail.com>
2022-06-20 14:06:11 -0400

    Update Likes Controller to add create/destroy

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,13 +1,23 @@
 class LikesController < ApplicationController
-  before_action :set_user, only: %i[ new edit update destroy ]
   before_action :authenticate_user!
+
+  def create
+    params[:like][:liked_by] = current_user.id
+    @like = Like.new(like_params)
+    if @like.save
+      flash[:success] = 'Thanks for liking!'
+    else
+      flash[:alert] = @like.errors.full_messages.join(', ')
+    end
+  end
+
+  def destroy
+    @like = current_user.likes.find(params[:id])
+  end
 
   private
 
-  def set_user
-    @user = User.find(current_user.id)
-  end
-
   def like_params
+    params.require(:like).permit(:liked_by, :likeable_id, :likeable_type)
   end
 end
