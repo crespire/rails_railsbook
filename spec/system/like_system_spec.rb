86854ad02fdac93a0790e41e3cc4bb671ea8c40b
crespire <li.simmon@gmail.com>
2022-06-27 14:26:46 -0400

    Reogranize Like System Tests
    * Grouped by liked already vs not
    * Tests pass using database level creation, have to update to tests to use views.

--- spec/system/like_system_spec.rb
+++ spec/system/like_system_spec.rb
@@ -5,37 +5,63 @@ RSpec.describe 'Likes system', type: :system do
     driven_by(:selenium_chrome_headless)
   end
 
-  context 'where there are posts by different users' do
-    let!(:user1) { FactoryBot.create(:user_with_posts) }
-    let!(:user2) { FactoryBot.create(:user_with_posts) }
+  context 'where there are posts by two users' do
+    let!(:user1) { FactoryBot.create(:user, :with_posts, post_count: 1) }
+    let!(:post) { FactoryBot.create(:post) }
 
-    it 'allows a user to like their own posts' do
+    context 'where one user does not have any likes' do
+      it 'allows a user to like their own posts' do
+        post = create(:post, user: user1)
+        expect(post.likes.size).to eq(0)
+        create(:like, likeable: post, user: user1) # lol I have to create these likes via clicks
+      end
+
+      it "allows a user to like another user's post" do
+        expect(post.likes.size).to eq(0)
+        create(:like, likeable: post, user: user1)
+      end
+
+      after(:each) do
+        expect(Like.count).to eq(1)
+        expect(user1.likes_count).to eq(1)
+      end
     end
 
-    it 'allows a user to remove their like on their own post' do
-    end
+    context 'where one user has liked some content' do
+      let!(:user1) { FactoryBot.create(:user) }
+      let!(:comment) { FactoryBot.create(:comment) }
 
-    it "allows a user to like another user's post" do
-    end
+      before(:each) do
+        create(:like, likeable: comment, user: user1)
+      end
 
-    it "allows a user to remove their like on another user's post" do
+      xit 'allows a user to remove their like on their own post' do
+      end
+
+      xit "allows a user to remove their like on another user's post" do
+      end
     end
   end
 
-  context 'where there are comments by different users' do
-    it 'allows a user to like their own comments' do
+  xcontext 'where there are comments by two users' do
+
+    context 'where a user has not liked any content' do
+      it 'allows a user to like their own comments' do
+      end
+
+      it "allows a user to like another user's comment" do
+      end
     end
 
-    it 'allows a user to remove their like on their own comments' do
-    end
+    context 'where a user has already liked some content' do
+      it 'allows a user to remove their like on their own comments' do
+      end
 
-    it "allows a user to like another user's comment" do
-    end
-
-    it "allows a user to remove their own like on another user's comment" do
+      it "allows a user to remove their own like on another user's comment" do
+      end
     end
   end
 
-  context 'Correctly deletes likes when the parent resource is deleted' do
+  xcontext 'Correctly deletes likes when the parent resource is deleted' do
   end
 end
