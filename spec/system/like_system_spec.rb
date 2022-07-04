ae1bbddb351ae1c474f87f7608c7c44e55588031
crespire <li.simmon@gmail.com>
2022-07-04 14:32:40 -0400

    Complete system tests for post liking and unliking

--- spec/system/like_system_spec.rb
+++ spec/system/like_system_spec.rb
@@ -2,43 +2,79 @@ require 'rails_helper'
 
 RSpec.describe 'Likes system', type: :system do
   before do
-    driven_by(:selenium_chrome_headless)
+    driven_by(:selenium_chrome)
   end
 
   context 'where there are posts by two users' do
     let!(:user1) { FactoryBot.create(:user, :with_posts, post_count: 1) }
-    let!(:post) { FactoryBot.create(:post) }
 
     context 'where one user does not have any likes' do
       it 'allows a user to like their own posts' do
-        post = create(:post, user: user1)
-        expect(post.likes.size).to eq(0)
-        create(:like, likeable: post, user: user1) # lol I have to create these likes via clicks
+        post_self = user1.posts.first
+        expect(Like.count).to eq(0)
+        expect(post_self.likes.size).to eq(0)
+
+        login_as(user1)
+        visit root_path
+
+        find('div.c-post__content', text: post_self.content).sibling('div.c-post__status').click_link('Like')
+
+        expect(page).to have_text('Likes: 1')
+        post_self.reload
+        expect(Like.count).to eq(1)
+        expect(post_self.likes.size).to eq(1)
       end
 
       it "allows a user to like another user's post" do
+        post = create(:post, content: 'Capybara Test Post')
+        expect(Like.count).to eq(0)
         expect(post.likes.size).to eq(0)
-        create(:like, likeable: post, user: user1)
-      end
 
-      after(:each) do
+        login_as(user1)
+        visit root_path
+
+        find('div.c-post__content', text: 'Capybara Test Post').sibling('div.c-post__status').click_link('Like')
+
+        expect(page).to have_text('Likes: 1')
+        post.reload
         expect(Like.count).to eq(1)
-        expect(user1.likes_count).to eq(1)
+        expect(post.likes.size).to eq(1)
       end
     end
 
     context 'where one user has liked some content' do
-      let!(:user1) { FactoryBot.create(:user) }
-      let!(:comment) { FactoryBot.create(:comment) }
+      let!(:user1) { FactoryBot.create(:user, :with_posts, post_count: 1) }
+      let!(:post) { FactoryBot.create(:post) }
 
-      before(:each) do
-        create(:like, likeable: comment, user: user1)
+      it 'allows a user to remove their like on their own post' do
+        post_self = user1.posts.first
+        create(:like, likeable: post_self, user: user1)
+        expect(post_self.likes.size).to eq(1)
+
+        login_as(user1, scope: :user)
+        visit root_path
+
+        find('div.c-post__content', text: post_self.content).sibling('div.c-post__status').click_link('Unlike')
+
+        expect(page).to have_text('Likes: 0')
+        post_self.reload
+        expect(Like.count).to eq(0)
+        expect(post_self.likes.size).to eq(0)
       end
 
-      xit 'allows a user to remove their like on their own post' do
-      end
+      it "allows a user to remove their like on another user's post" do
+        create(:like, likeable: post, user: user1)
+        expect(post.likes.size).to eq(1)
 
-      xit "allows a user to remove their like on another user's post" do
+        login_as(user1, scope: :user)
+        visit root_path
+
+        find('div.c-post__content', text: post.content).sibling('div.c-post__status').click_link('Unlike')
+
+        expect(page).to have_text('Likes: 0')
+        post.reload
+        expect(Like.count).to eq(0)
+        expect(post.likes.size).to eq(0)
       end
     end
   end
