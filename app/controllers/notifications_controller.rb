8c68e482344bb7fd98bfacf00fae1b607bb1dd98
crespire <li.simmon@gmail.com>
2022-07-05 16:17:10 -0400

    Update notification controller to only have Index method.

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,5 +1,8 @@
 class NotificationsController < ApplicationController
+  before_action :authenticate_user!
+
   def index
-    @notifications = current_user.notifications.all
+    @new = current_user.notifications.unseen
+    @old = current_user.notifications.seen
   end
 end
