36e9fecd925102478ce0cffc9b4ef93b5111947c
crespire <li.simmon@gmail.com>
2022-07-05 17:37:56 -0400

    Update each trigger type to add callbacks to generate notifications

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,8 +1,26 @@
 class Comment < ApplicationRecord
+  after_create :notify
+  after_destroy :remove_notify
+
   belongs_to :post, counter_cache: true
   belongs_to :user, counter_cache: true
 
   has_many :likes, as: :likeable, dependent: :destroy
 
   validates :content, presence: true
+
+  private
+
+  def notify
+    parent = Post.find(post.id)
+    message = "#{user.name} added a comment to your post."
+    Notification.create(user_id: parent.user.id, trigger_type: 'Comment', trigger_id: id, triggered_by_id: user.id, message: message)
+  end
+
+  def remove_notify
+    parent = Post.find(post.id)
+    return unless Notification.where(id: id, trigger_type: 'Comment', trigger_id: id, user_id: parent.user.id).exists?
+
+    Notification.destroy_by(id: id, trigger_type: 'Comment', trigger_id: id, user_id: parent.user.id)
+  end
 end
--- app/models/request.rb
+++ app/models/request.rb
@@ -1,4 +1,7 @@
 class Request < ApplicationRecord
+  after_create :notify
+  after_destroy :remove_notify
+
   belongs_to :user
   belongs_to :friend, class_name: :User
 
@@ -12,5 +15,18 @@ class Request < ApplicationRecord
   def accept_request
     self.accepted = true
     self.save
+  end
+
+  private
+
+  def notify
+    message = "#{user.name} sent you a friend request."
+    Notification.create(user_id: friend.id, trigger_type: 'Request', trigger_id: id, triggered_by_id: user.id, message: message)
+  end
+
+  def remove_notify
+    return unless Notification.where(id: id, trigger_type: 'Request', trigger_id: id, user_id: friend.id).exists?
+
+    Notification.destroy_by(id: id, trigger_type: 'Request', trigger_id: id, user_id: friend.id)
   end
 end
