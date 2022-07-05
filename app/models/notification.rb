e63418f036a63235c3430dd3d14992eecfc635d1
crespire <li.simmon@gmail.com>
2022-07-05 14:33:45 -0400

    Add a seen boolean to notifications and update model

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,3 +1,13 @@
 class Notification < ApplicationRecord
   belongs_to :user, counter_cache: true
+
+  scope :new, -> { where('seen = false') }
+  scope :seen, -> { where('seen = true') }
+
+  def mark_seen
+    return if seen
+
+    self.seen = true
+    save
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_22_183637) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_05_182845) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -39,6 +39,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_22_183637) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "seen", default: false
     t.index ["user_id"], name: "index_notifications_on_user_id"
   end
 
