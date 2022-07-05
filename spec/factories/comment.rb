613fc0ec8de924696675015b7cb1599a28422219
crespire <li.simmon@gmail.com>
2022-07-05 14:15:17 -0400

    Update comment factory to use less text

--- spec/factories/comment.rb
+++ spec/factories/comment.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :comment do
-    content { Faker::Hipster.paragraphs(number: 2).join }
+    content { Faker::Hipster.sentence(word_count: 4) }
 
     association :user
     association :post
