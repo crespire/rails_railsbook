a883c304d33a989c8cb76323cf4a921796845a4a
crespire <li.simmon@gmail.com>
2022-07-07 11:02:03 -0400

    Add Notifiable concern

---
+++ app/models/concerns/notifiable.rb
@@ -0,0 +1,30 @@
+module Notifiable
+  extend ActiveSupport::Concern
+
+  included do
+    def set_details
+      @details ||= Hash.new
+
+      case self
+      when Request
+        @details[:notify_target] ||= friend
+        @details[:message] ||= "#{user.name} sent you a friend request."
+      when Like
+        @details[:notify_target] ||= likeable.user
+        @details[:message] ||= "#{user.name} liked your #{likeable_type.downcase}."
+      when Comment
+        @details[:notify_target] ||= post.user
+        @details[:message] ||= "#{user.name} commented on your post."
+      else
+        raise 'Invalid notify type.'
+      end
+    end
+
+    def notify
+      set_details
+      return unless set_details.length == 2
+
+      Notification.create(actor: user, notifiable: self, target: @details[:notify_target], message: @details[:message])
+    end
+  end
+end
