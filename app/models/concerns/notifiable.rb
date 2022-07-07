e6f8a6517a9e807bd19aaa2314d21f8392b7edf9
crespire <li.simmon@gmail.com>
2022-07-07 15:34:45 -0400

    Changed from after_initialize to after_commit, so the parent object is always valid

--- app/models/concerns/notifiable.rb
+++ app/models/concerns/notifiable.rb
@@ -2,27 +2,35 @@ module Notifiable
   extend ActiveSupport::Concern
 
   included do
+    attr_reader :details
+
+    after_commit do
+      set_details
+    end
+
+    def notify
+      Notification.create(actor: user, notifiable: self, target: @details[:notify_target], message: @details[:message])
+    end
+
+    private
+
     def set_details
       @details ||= Hash.new
+      @details[:message] ||= "#{user.name}"
 
       case self
       when Request
         @details[:notify_target] ||= friend
-        @details[:message] ||= "#{user.name} sent you a friend request."
+        @details[:message] += ' sent you a friend request.'
       when Like
         @details[:notify_target] ||= likeable.user
-        @details[:message] ||= "#{user.name} liked your #{likeable_type.downcase}."
+        @details[:message] += " liked your #{likeable_type.downcase}."
       when Comment
         @details[:notify_target] ||= post.user
-        @details[:message] ||= "#{user.name} commented on your post."
+        @details[:message] += ' commented on your post.'
       else
         raise 'Invalid notify type.'
       end
-    end
-
-    def notify
-      set_details
-      Notification.create(actor: user, notifiable: self, target: @details[:notify_target], message: @details[:message])
     end
   end
 end
