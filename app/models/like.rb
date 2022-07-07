75ac000f67cf527d7a534d5a8daf516eb0d92b51
crespire <li.simmon@gmail.com>
2022-07-07 11:01:45 -0400

    Update models to support Notifiable

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,6 @@
 class Like < ApplicationRecord
+  include Notifiable
+
   belongs_to :likeable, polymorphic: true, counter_cache: true
   belongs_to :user, foreign_key: :liked_by, counter_cache: true
 
@@ -6,19 +8,4 @@ class Like < ApplicationRecord
   validates :likeable_type, presence: true
   validates :likeable_id, presence: true
   validates :liked_by, uniqueness: { scope: %i[likeable_id likeable_type] }
-
-  private
-
-  def notify
-    resource = likeable_type.constantize.find(likeable_id)
-    message = "#{user.name} liked your #{likeable_type.downcase}."
-    Notification.create(user_id: resource.user.id, trigger_type: 'Like', trigger_id: id, triggered_by_id: liked_by, message: message)
-  end
-
-  def remove_notify
-    resource = likeable_type.constantize.find(likeable_id)
-    return unless Notification.where(id: id, trigger_type: 'Like', trigger_id: id, user_id: resource.user.id).exists?
-
-    Notification.destroy_by(id: id, trigger_type: 'Like', trigger_id: id, user_id: resource.user.id)
-  end
 end
--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,19 +1,19 @@
 class Notification < ApplicationRecord
-  belongs_to :notifiable, polymorphic: true, counter_cache: true
-  belongs_to :user, foreign_key: :target_id, counter_cache: true
+  belongs_to :notifiable, polymorphic: true
+  belongs_to :target, class_name: 'User', counter_cache: true
+  belongs_to :actor, class_name: 'User'
 
   validates :target_id, presence: true
   validates :notifiable_type, presence: true
   validates :notifiable_id, presence: true
   validates :message, presence: true
 
-  scope :unseen, -> { where('seen = false') }
-  scope :seen, -> { where('seen = true') }
+  scope :unseen, -> { where(seen: false) }
+  scope :seen, -> { where(seen: true) }
 
   def mark_seen
     return if seen
 
-    self.seen = true
-    save
+    update_attribute(:seen, true)
   end
 end
