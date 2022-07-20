6b1f9dd2dfab619f0a3a9a7d70592959ec019672
crespire <li.simmon@gmail.com>
2022-07-20 01:19:30 -0400

    Update already_liked? to use different approach

--- app/models/user.rb
+++ app/models/user.rb
@@ -54,7 +54,7 @@ class User < ApplicationRecord
   end
 
   def already_liked?(resource)
-    self.likes.include?(resource)
+    self.likes.any? { |like| resource.likes.include?(like) }
   end
 
   def self.from_omniauth(access_token)
