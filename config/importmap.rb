d5631c4d1a11afdb9431fc32b8ec67a7a6607ae8
crespire <li.simmon@gmail.com>
2022-07-26 09:01:07 -0400

    Add JS libraries for ActionText

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,8 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
+
+# Add JS for trix and actiontext plus dependencies.
+pin "trix", preload: true # @2.0.0
+pin "@rails/actiontext", to: "@rails--actiontext.js", preload: true # @7.0.3
+pin "@rails/activestorage", to: "@rails--activestorage.js", preload: true # @7.0.3
