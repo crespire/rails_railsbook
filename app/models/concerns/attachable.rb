95d8c89549648147883beaf3802b782f4e8ea034
crespire <li.simmon@gmail.com>
2022-07-26 10:57:05 -0400

    Updates to N+1 queries and displays

--- app/models/concerns/attachable.rb
+++ app/models/concerns/attachable.rb
@@ -1,16 +1,18 @@
 module Attachable
   extend ActiveSupport::Concern
 
+  MAX_FILE_SIZE = 2_097_152 # 2MB
+
   included do
     def attachment_type_allowed
       rich_text_content.body.attachments do |attachment|
-        errors.add(:content_type, 'not supported, only images.') unless %w[image/jpeg image/png image/gif image/webp image/apng].include? attachment.content_type
+        errors.add(:content_type, 'not supported, only images.') unless attachment.image?
       end
     end
 
     def attachment_size_allowed
       rich_text_content.body.attachments do |attachment|
-        errors.add(:size, 'too large, 2MB or less.') unless attachment.byte_size <= 2097152
+        errors.add(:size, 'too large, 2MB or less.') unless attachment.byte_size <= MAX_FILE_SIZE
       end
     end
   end
