13e4526e64f223136df55ffcb757bc0db943bb16
crespire <li.simmon@gmail.com>
2022-07-27 13:32:35 -0400

    Update AWS Credentials

--- config/initializers/aws-sdk.rb
+++ config/initializers/aws-sdk.rb
@@ -1,6 +1,7 @@
 # config/initializers/aws-sdk.rb
 
-Aws.config.update(
-  credentials: Aws::Credentials.new('access-key-id', 'secret-access-key'),
+S3Client = Aws::S3::Client.new(
+  access_key_id: 'ACCESS_KEY_ID',
+  secret_access_key: 'SECRET_ACCESS_KEY',
   region: 'ca-central-1'
 )
