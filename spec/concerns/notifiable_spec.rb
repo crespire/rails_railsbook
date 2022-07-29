a13fde5402bf576c246c2244bea7486eed68a739
crespire <li.simmon@gmail.com>
2022-07-29 14:31:33 -0400

    Update test vars to be more semantic

--- spec/concerns/notifiable_spec.rb
+++ spec/concerns/notifiable_spec.rb
@@ -3,20 +3,20 @@ require 'rails_helper'
 RSpec.describe Notifiable do
   context 'when the resource is a like' do
     let(:post) { create(:post) }
-    let(:test_resource) { create(:like, likeable: post) }
+    let(:like) { create(:like, likeable: post) }
 
     it 'correctly notifies the resource owner' do
-      expect { test_resource.notify }.to change { Notification.count }.by(1)
+      expect { like.notify }.to change { Notification.count }.by(1)
       expect(Notification.first.target).to eq(post.user)
     end
   end
 
   context 'when the resource is a comment' do
     let(:post) { create(:post) }
-    let(:test_resource) { create(:comment, post: post) }
+    let(:comment) { create(:comment, post: post) }
 
     it 'correctly notifies the post owner' do
-      expect { test_resource.notify }.to change { Notification.count }.by(1)
+      expect { comment.notify }.to change { Notification.count }.by(1)
       expect(Notification.first.target).to eq(post.user)
     end
   end
@@ -24,10 +24,10 @@ RSpec.describe Notifiable do
   context 'when the resource is a request' do
     let(:user_a) { create(:user) }
     let(:user_b) { create(:user) }
-    let(:test_resource) { create(:request, user: user_a, friend: user_b) }
+    let(:request) { create(:request, user: user_a, friend: user_b) }
 
     it 'correctly notifies the target user' do
-      expect { test_resource.notify }.to change { Notification.count }.by(1)
+      expect { request.notify }.to change { Notification.count }.by(1)
       expect(Notification.first.target).to eq(user_b)
     end
   end
