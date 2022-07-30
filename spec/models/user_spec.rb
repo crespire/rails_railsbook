2b4b978ae7896f278dce65d776c8626962151ff1
crespire <li.simmon@gmail.com>
2022-07-30 11:27:07 -0400

    Finish two tests to user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,13 +2,37 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   context 'given a user with a mix of friends and pending requests' do
-    it 'returns the right number of objects when sent #friends'
-  
-    it 'returns the right number of objects when sent #pending_friends'
-  
-    it 'returns the right number of objects when sent #all_friends'
-  
-    it 'returns the right number of objects when sent #all_requests'
+    let(:user) { create(:user) }
+    let(:friend) { create(:user) }
+    let(:pending) { create(:user) }
+
+    context 'when the user is the sender' do
+      it 'returns the right number of objects when sent #friends' do
+        create(:request, :accepted, user: user, friend: friend)
+        create(:request, user: user, friend: pending)
+        expect(user.reload.friends.length).to eq(1)
+      end
+    
+      it 'returns the right number of objects when sent #pending_friends' do
+        create(:request, :accepted, user: user, friend: friend)
+        create(:request, user: user, friend: pending)
+        expect(user.reload.pending_friends.length).to eq(1)
+      end
+    
+      it 'returns the right number of objects when sent #all_friends'
+    
+      it 'returns the right number of objects when sent #all_requests'
+    end
+
+    context 'when the user is the receiver' do
+      it 'returns the right number of objects when sent #friends'
+    
+      it 'returns the right number of objects when sent #pending_friends'
+    
+      it 'returns the right number of objects when sent #all_friends'
+    
+      it 'returns the right number of objects when sent #all_requests'
+    end
   
     context 'when using #find_request' do
       it 'returns nil when passed self'
