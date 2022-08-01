580a83dd84b563fcb483baf6371693feb5930ea4
crespire <li.simmon@gmail.com>
2022-08-01 17:37:59 -0400

    Remove dupes from app.js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,9 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-import 'trix'
-import '@rails/actiontext'
 import '@rails/activestorage'
-import "trix"
 import "@rails/actiontext"
+import "trix"
 import 'trix-editor-overrides'
