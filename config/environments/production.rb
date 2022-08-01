088fa8f84dc82319cba2a69d8411339743ca23ac
crespire <li.simmon@gmail.com>
2022-08-01 17:26:21 -0400

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,6 +30,9 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
+  # Load assets with fingerprint behavior
+  config.assets.digest = true
+
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
 
