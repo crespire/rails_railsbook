52f3dae22c36d91bfb4c8787213de8b617f94a4c
crespire <li.simmon@gmail.com>
2022-08-01 16:18:30 -0400

    Revert to dig

--- config/initializers/aws-sdk.rb
+++ config/initializers/aws-sdk.rb
@@ -2,5 +2,5 @@ require 'aws-sdk-core'
 
 Aws.config.update(
   region: 'can-central-1',
-  credentials: Aws::Credentials.new(Rails.application.credentials.aws.access_key_id, Rails.application.credentials.aws.secret_access_key)
+  credentials: Aws::Credentials.new(Rails.application.credentials.dig(:aws, :access_key_id), Rails.application.credentials.dig(:aws, :secret_access_key))
 )
--- config/initializers/sendgrid.rb
+++ config/initializers/sendgrid.rb
@@ -4,7 +4,7 @@ if(Rails.env.production?)
     :port           => '587',
     :authentication => :plain,
     :user_name      => 'apikey',
-    :password       => Rails.application.credentials.sendgrid_key,
+    :password       => Rails.application.credentials.dig(:sendgrid_key),
     :domain         => 'crespire.dev'
   }
   ActionMailer::Base.delivery_method = :smtp
