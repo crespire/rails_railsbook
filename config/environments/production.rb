eb3f2c568e466e50c495a223b04b97d75afe785e
crespire <li.simmon@gmail.com>
2022-08-02 14:44:23 -0400

    Add default URL option to mailer for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,6 +71,9 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  # Set default host for mailer
+  config.action_mailer.default_url_options = { host: 'petbook.crespire.dev' }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
