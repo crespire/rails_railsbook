c4cc94a21d27648fac832ea47d5e4182dd03169e
crespire <li.simmon@gmail.com>
2022-08-02 11:58:39 -0400

    Add default url option to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,6 +16,10 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  # Add default URL to render images on Turbo Streams properly
+  config.action_controller.default_url_options = { host: 'petbook.crespire.dev', protocol: 'https' }
+
+
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
   # config.require_master_key = true
