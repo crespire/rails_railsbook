2bb748e2009fe9ac7e0d5b1fef253479e5e337aa
crespire <li.simmon@gmail.com>
2022-08-04 11:55:45 -0400

    Add request validation to self-friend, and update request index to use different list method

--- app/models/request.rb
+++ app/models/request.rb
@@ -9,11 +9,18 @@ class Request < ApplicationRecord
   validates :user_id, presence: true
   validates :friend_id, presence: true
   validates :user_id, uniqueness: { scope: [:friend_id] }
+  validate :friend_self
 
   scope :accepted, -> { where('accepted = true') }
   scope :pending, -> { where('accepted = false') }
 
   def accept_request
     update!(accepted: true)
+  end
+
+  private
+
+  def friend_self
+    errors.add(:friend, "can't be yourself.") unless user != friend
   end
 end
