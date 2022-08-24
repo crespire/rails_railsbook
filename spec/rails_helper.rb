59f4f8d0dc1000294a546d341086c96ecf7ddd27
crespire <li.simmon@gmail.com>
2022-08-24 12:22:50 -0400

    Add config options for SimpleCov

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,3 +1,9 @@
+require 'simplecov'
+SimpleCov.start do
+  add_filter "/spec/"
+  add_filter "/config/"
+end
+
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
